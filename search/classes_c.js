d0a2bc3cff415ab619148a9e78893a35806d66f1
agners <agners@users.noreply.github.com>
2022-06-09 13:44:32 +0000

    deploy: 9cfb91e73fe50886f50e63e8271474cf37d993f1

--- search/classes_c.js
+++ search/classes_c.js
@@ -1,5 +1,9 @@
 var searchData=
 [
-  ['server_753',['Server',['../df/d24/classSimpleWeb_1_1Server.html',1,'SimpleWeb']]],
-  ['steeringdata_754',['SteeringData',['../d6/de8/classotbr_1_1SteeringData.html',1,'otbr']]]
+  ['registration_796',['Registration',['../da/d01/structotbr_1_1DBus_1_1SrpServerInfo_1_1Registration.html',1,'otbr::DBus::SrpServerInfo::Registration'],['../d3/dcf/classotbr_1_1Mdns_1_1Publisher_1_1Registration.html',1,'otbr::Mdns::Publisher::Registration']]],
+  ['request_797',['Request',['../da/df0/classotbr_1_1rest_1_1Request.html',1,'otbr::rest']]],
+  ['resource_798',['Resource',['../d7/d6e/classotbr_1_1rest_1_1Resource.html',1,'otbr::rest']]],
+  ['response_799',['Response',['../d2/db0/classotbr_1_1rest_1_1Response.html',1,'otbr::rest']]],
+  ['responsecounters_800',['ResponseCounters',['../d9/d29/structotbr_1_1DBus_1_1SrpServerInfo_1_1ResponseCounters.html',1,'otbr::DBus::SrpServerInfo']]],
+  ['restwebserver_801',['RestWebServer',['../d8/d5d/classotbr_1_1rest_1_1RestWebServer.html',1,'otbr::rest']]]
 ];
--- search/classes_d.js
+++ search/classes_d.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['taskrunner_755',['TaskRunner',['../dd/d62/classotbr_1_1TaskRunner.html',1,'otbr']]],
-  ['threadapidbus_756',['ThreadApiDBus',['../df/dcc/classotbr_1_1DBus_1_1ThreadApiDBus.html',1,'otbr::DBus']]],
-  ['threadhelper_757',['ThreadHelper',['../d4/d44/classotbr_1_1agent_1_1ThreadHelper.html',1,'otbr::agent']]],
-  ['tlv_758',['Tlv',['../d0/d02/classotbr_1_1Tlv.html',1,'otbr']]],
-  ['txtentry_759',['TxtEntry',['../d0/dee/structotbr_1_1Mdns_1_1Publisher_1_1TxtEntry.html',1,'otbr::Mdns::Publisher::TxtEntry'],['../d2/d60/structotbr_1_1DBus_1_1TxtEntry.html',1,'otbr::DBus::TxtEntry']]]
+  ['server_802',['Server',['../df/d24/classSimpleWeb_1_1Server.html',1,'SimpleWeb']]],
+  ['serviceregistration_803',['ServiceRegistration',['../da/da8/classotbr_1_1Mdns_1_1Publisher_1_1ServiceRegistration.html',1,'otbr::Mdns::Publisher']]],
+  ['srpserverinfo_804',['SrpServerInfo',['../d9/dc0/structotbr_1_1DBus_1_1SrpServerInfo.html',1,'otbr::DBus']]],
+  ['steeringdata_805',['SteeringData',['../d6/de8/classotbr_1_1SteeringData.html',1,'otbr']]]
 ];
